4723a5d01192a0d5faeae19583e0b637c0d7bf44
james griggs <griggsjames0806@gmail.com>
2020-07-28 23:52:48 -0400

    latest push

--- p0_project/PizzaBox.Testing/UnitTest1.cs
+++ p0_project/PizzaBox.Testing/UnitTest1.cs
@@ -1,14 +1,26 @@
-using System;
+using PizzaBox.Domain.Models;
+using System.Collections.Generic;
 using Xunit;
 
-namespace PizzaBox.Testing
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
 
-        }
+namespace PizzaStore.Testing.Tests
+{
+  public class OrderTest
+  {
+    [Theory]
+    [InlineData("N","S","C","t",10.00)]
+    public void Test_AddPizza(string n, string c, string s, string  t, double p )
+    {
+      
+      var sut = new Order();
+      string size = s;
+      string crust = c;
+      string name = n;
+      double price =p;
+      List<string> toppings = new List<string>{t};
+
+      sut.AddPizza(name, crust, size, toppings, price );
+      Assert.True(sut.Pizzas.Count > 0);
     }
+  }
 }
